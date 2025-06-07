6b651c2fe3a85337d8bcefc7cf0bf23dcc0a876a
RubenMFonte <a919.ruben@gmail.com>
2025-06-07 21:02:16 +0100

    Add simple index page and GET method processing

--- src/main/java/com/falconserver/FalconServer/Main.java
+++ src/main/java/com/falconserver/FalconServer/Main.java
@@ -2,10 +2,22 @@ package com.falconserver.FalconServer;
 
 import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
 
 public class Main {
 	
 	final static int PORT = 8080;
+	
+	final static String CONTENTTYPE_PLAIN = "text/plain";
+	final static String CONTENTTYPE_HTML = "text/html";
+	final static String CONTENTTYPE_CSS = "text/css";
+	final static String CONTENTTYPE_DEFAULT = "application/octet-stream";
+	
+	final static String STATUSCODE_200 = "200 OK";
+	final static String STATUSCODE_400 = "400 Bad Request";
+	final static String STATUSCODE_404 = "404 Not Found";
+	final static String STATUSCODE_405 = "405 Method Not Allowed";
+	final static String STATUSCODE_500 = "500 Internal Server Error";
 	
 	public static void main(String[] args) {
 
@@ -23,16 +35,27 @@ public class Main {
 					
 					String request = in.readLine();
 					
-					String message = "Received = " + request;
+					if(request == null) return;
+
+					String[] requestParts = request.split(" ");
 					
-					String response = "HTTP/1.1 200 OK\r\n" +
-	                        "Content-Type: text/plain\r\n" +
-	                        "Content-Length: " + message.length() + "\r\n" +
-	                        "Connection: close\r\n" +
-	                        "\r\n" +
-	                        message;
+					if(requestParts.length != 3) {
+						out.write(getHttpResponse(STATUSCODE_400, CONTENTTYPE_PLAIN, "Request with incorrect format!"));
+					}
+					else {
+						
+						String method = requestParts[0];
+						
+						if (!method.equals("GET")) {
+						    out.write(getHttpResponse(STATUSCODE_405, CONTENTTYPE_PLAIN, "Method is not supported."));
+						    return;
+						}
+						
+						String path = requestParts[1];	
+						
+						out.write(getFileContent(path));
+					}
 					
-					out.write(response);
 					out.flush();
 					
 				}
@@ -47,6 +70,59 @@ public class Main {
 			System.out.println("Error on server start/nMessage: " + ex.getMessage());
 		}
 		
+	}
+	
+	private static String getFileContent(String path) {
+		
+		if(path.equals("/")) {
+			path = "/index.html";
+		}
+		else {
+			// Check for path out of directory bounds
+			try {
+				String canonicalPath = new File("public", path).getCanonicalPath();
+				String publicDirPath = new File("public").getCanonicalPath();
+				
+				if(!canonicalPath.startsWith(publicDirPath)) {
+					return getHttpResponse(STATUSCODE_404, CONTENTTYPE_PLAIN, "Page not found!");
+				}
+			} catch (IOException e) {
+				return getHttpResponse(STATUSCODE_500, CONTENTTYPE_PLAIN, "Internal Server Error.");
+			}
+		}
+		
+		System.out.println("Path: " + path);
+
+		File file = new File("public", path.equals("/") ? "/index.html" : path.substring(1));
+		
+		if(file.exists() && !file.isDirectory()) {
+			try {
+				String body = Files.readString(file.toPath());
+				String contentType = getContentType(file.getName());
+				
+				return getHttpResponse(STATUSCODE_200, contentType, body);
+			}
+			catch (IOException ex) {
+				System.out.println("Error reading file/nMessage: " + ex.getMessage());
+				return getHttpResponse(STATUSCODE_500, CONTENTTYPE_PLAIN, "Internal Server Error.");
+			}
+		}
+		else return getHttpResponse(STATUSCODE_404, CONTENTTYPE_PLAIN, "Page not found!");
+	}
+	
+	private static String getHttpResponse(String status, String contentType, String content) {
+		return "HTTP/1.1 " + status + "\r\n" +
+		        "Content-Type: " + contentType + "\r\n" +
+		        "Content-Length: " + content.length() + "\r\n" +
+		        "Connection: close\r\n" +
+		        "\r\n" +
+		        content;
+	}
+	
+	private static String getContentType(String filename) {
+		if(filename.endsWith(".html")) return CONTENTTYPE_HTML;
+		if(filename.endsWith(".css")) return CONTENTTYPE_CSS;
+		return CONTENTTYPE_DEFAULT;
 	}
 
 }
